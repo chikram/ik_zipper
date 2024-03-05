5fdda7827084cbe880b1a725751e7fb3513c4dda
Ikram Aslam <ikramaslam1020@gmail.com>
2024-03-06 01:22:21 +0500

    Adding google fontfize and adding navbar

---
+++ src/components/Nav.js
@@ -0,0 +1,251 @@
+import React from 'react'
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+
+            <div className="container-fluid">
+
+                <button
+                    className="navbar-toggler px-0"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarExample5"
+                    aria-controls="navbarExample5"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarExample5">
+
+                    <ul className="navbar-nav ps-lg-0 mx-0">
+                        <li className="nav-item">
+                            <a className="navbar-brand text-center fw-bold" href="#">IK <span className='text-danger'>Zipper</span></a>
+                        </li>
+
+                        <li className="nav-item dropdown position-static">
+                            <a
+                                className="nav-link"
+                                href="#"
+                                id="navbarDropdown"
+                                role="button"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <span className=" text-dark">Our Product</span>
+                                <span className="plus-icon d-lg-none">&#43;</span>
+                            </a>
+
+                            <div
+                                className="dropdown-menu w-100 mt-0 border-0"
+                                aria-labelledby="navbarDropdown"
+                            >
+                                <div className="container">
+                                    <div className="row my-4">
+                                        <div className="col-md-8 col-lg-4 mb-4 mb-lg-0">
+                                            <p className="text-uppercase font-weight-bold cd">
+                                                Brand Name
+                                            </p>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div className="col-md-8 col-lg-4 mb-4 mb-lg-0">
+                                            <p className="text-uppercase font-weight-bold cd">
+                                                Brand Name
+                                            </p>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                        <div className="col-md-8 col-lg-4 mb-4 mb-lg-0">
+                                            <p className="text-uppercase font-weight-bold cd">
+                                                Brand Name
+                                            </p>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                            <a href="" className="text-muted text-decoration-none">
+                                                <div className="row mb-2">
+                                                    <div className="col">
+                                                        <img
+                                                            src="sho.PNG"
+                                                            alt=""
+                                                            width="50"
+                                                            height="50"
+                                                            className="p-2"
+                                                        />
+                                                        <span className="mb-2 b">Shoes</span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark " aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" aria-current="page" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" aria-current="page" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </nav>
+    )
+}
+
+export default Nav
